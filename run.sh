fc59faf6b6d164b135c456ba8ac3696867cd5bc5
Chris Yoon <33583101+thechrisyoon08@users.noreply.github.com>
2018-09-26 20:11:58 -0400

    run iris

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-g++ -std=c++11 xor_test.cpp ./src/layer.cpp ./src/ann.cpp ./src/matrix_operations.cpp -o xor
-./xor
+g++ -std=c++11 iris_test.cpp ./src/layer.cpp ./src/ann.cpp ./src/matrix_operations.cpp ./src/back_propagation.cpp -o iris
