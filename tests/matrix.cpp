d4456f3ec9193db36f3cf43f21d0bdc554b654f0
Christopher Yoon <cyoon20@cgps.org>
2018-12-29 14:24:16 -0500

    add test directory

---
+++ tests/matrix.cpp
@@ -0,0 +1,44 @@
+#include "../include/matrix_operations.h"
+#include <iostream>
+
+int main(){
+    matrix::Matrix A(3, 4);
+    A.fill_weights("glorot");
+    std::cout << "\n--A--\n";
+    std::cout << A;
+    std::cout << "\n";
+
+    matrix::Matrix B(3, 4);
+    B.fill_weights("glorot");
+    std::cout << "\n--B--\n";
+    std::cout << B;
+    std::cout << "\n";
+
+    matrix::Matrix C(4, 3);
+    C.fill_weights("glorot");
+    std::cout << "\n--C--\n";
+    std::cout << C;
+    std::cout << "\n";
+
+    double scalar = 3.0;
+
+    std::cout << "\n--matrix addition--\n";
+    std::cout << A + B;
+    std::cout << "\n";
+
+    std::cout << "\n--matrix subtraction--\n";
+    std::cout << A - B;
+    std::cout << "\n";
+
+    std::cout << "\n--matrix multiplication--\n";
+    std::cout << A * C;
+    std::cout << "\n";
+
+    std::cout << "\n--scalar multiplication--\n";
+    std::cout << scalar * A;
+    std::cout << "\n";
+    // A.T();
+    // std::cout << "\n--Transpose--\n";
+    // std::cout << A;
+    return 0;
+}
