cf2ce7f03014e471511bdf51a78c10ec297a9106
Christopher Yoon <cyoon20@cgps.org>
2018-12-28 22:40:05 -0500

    refactor matrix operations

--- include/matrix_operations.h
+++ include/matrix_operations.h
@@ -1,34 +1,67 @@
 #pragma once
 
 #include <vector>
-#include "ann.h"
-#include "layer.h"
+#include <iostream>
 
-//initialize zero vector of dimensions x, y
-std::vector< std::vector<double> > zero_vector(int x, int y);
+namespace matrix{
 
-//initialize zero vector of same dimensions as input vector
-std::vector< std::vector<double> > zeros_like(std::vector< std::vector<double> > a);
+    struct Matrix{
+        Matrix(size_t row, size_t col){
+            m = row;
+            n = col;
+            matrix.resize(row * col);
+        }
+    
+        Matrix(){}
+    
+        size_t m; // rows
+        size_t n; // columns
+        vector<double> matrix; // row major matrix
+        bool transposed = false;
+        void fill_weights(std::string initializer); // weight initialization using "glorot," "he-et-al," and "kaiming" 
+        void fill_weights(double low, double high); // weight initialization with uniform distribution given lower and upper bound
+        void T(); //transpose
+        friend std::ostream& operator<<(std::ostream& os, const Matrix& M);
+    };
 
-//return random real number in range [-1, 1]
-double random_real(Layer layer);
+    // element wise addition
+    Matrix operator+(const Matrix& lhs, const Matrix& rhs);
 
-//initialize 2D vector of dimensions x, y with random doubles in [0,1]
-std::vector< std::vector<double> > random_vector(int x, int y, Layer layer);
+    // element wise subtraction
+    Matrix operator-(const Matrix& lhs, const Matrix& rhs);
+    
+    // scalar multiplication
+    Matrix operator*(const double k, const Matrix& a);
 
-//calculate dot product
-double dot(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b) ;
-
-//matrix multiplication
-std::vector< std::vector<double> > multiply(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b) ;
-
-std::vector< std::vector<double> > transpose(std::vector< std::vector<double> > a);
-
-std::vector< std::vector<double> > element_wise_add(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b);
-
-std::vector< std::vector<double> > element_wise_subtract(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b);
+    // matrix multipication (also dot product)
+    Matrix operator*(const Matrix& lhs, const Matrix& rhs);
 
 
-std::vector< std::vector<double> > element_wise_multiply(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b);
+    // std::vector< std::vector<double> > zero_vector(int x, int y);
 
-std::vector< std::vector<double> > scalar_multiply(double s, std::vector< std::vector<double> > a);
+    // //initialize zero vector of same dimensions as input vector
+    // std::vector< std::vector<double> > zeros_like(std::vector< std::vector<double> > a);
+
+    // //return random real number in range [-1, 1]
+    // double random_real(Layer layer);
+
+    // //initialize 2D vector of dimensions x, y with random doubles in [0,1]
+    // std::vector< std::vector<double> > random_vector(int x, int y, Layer layer);
+
+    // //calculate dot product
+    // double dot(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b) ;
+
+    // //matrix multiplication
+    // std::vector< std::vector<double> > multiply(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b) ;
+
+    // std::vector< std::vector<double> > transpose(std::vector< std::vector<double> > a);
+
+    // std::vector< std::vector<double> > element_wise_add(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b);
+
+    // std::vector< std::vector<double> > element_wise_subtract(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b);
+
+
+    // std::vector< std::vector<double> > element_wise_multiply(std::vector< std::vector<double> > a, std::vector< std::vector<double> > b);
+
+    // std::vector< std::vector<double> > scalar_multiply(double s, std::vector< std::vector<double> > a);
+};
