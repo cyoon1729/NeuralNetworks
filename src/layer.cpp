99f61f29d8947d370ca6509f32e420021b3fc18b
Christopher Yoon <cyoon20@cgps.org>
2018-12-31 19:42:37 -0500

    refactor layer class. backprop is not done

--- src/layer.cpp
+++ src/layer.cpp
@@ -1,36 +1,77 @@
 #include "../include/layer.h"
 #include "../include/matrix_operations.h"
+#include "../include/activation_functions.h"
 #include <vector>
 #include <string>
 #include <iostream>
+#include <assert.h>
 
 namespace neuralnet{
 
-// activate neuron using activation function
-matrix::Matrix Layer::activate(){
-    const matrix::Matrix activated_neurons;
-    return activated_neurons;
+void Layer::set_activation_function(){
+    if(this->activation_function_name == "linear"){
+        this->activation_function = linear;
+        this->activation_function_derivative = linear_derivative;
+    }else if(this->activation_function_name == "sigmoid"){
+        this->activation_function = sigmoid;
+        this->activation_function_derivative = sigmoid_derivative;
+    }else if(this->activation_function_name == "relu"){
+        this->activation_function = relu;
+        this->activation_function_derivative = relu;
+    }else if(this->activation_function_name == "tanh"){
+        this->activation_function = hyperbolic_tan;
+        this->activation_function_derivative = hyperbolic_tan_derivative;
+    }else{
+        std::cout << "activation function is not recognized, setting to linear";
+        this->activation_function = linear;
+        this->activation_function_derivative = linear_derivative;
+    }
+}
 
+// perform forward propagate from layer to next layer
+void Layer::activate(){
+    this->activated_neurons = this->neurons;
+    for(auto& z : this->activated_neurons.matrix){
+        z = this->activation_function(z);
+    }
+}
+void Layer::forward(Layer &next_layer){
+    matrix::copy_matrix(this->transposed_weights, this->weights);
+    transposed_weights.T();
+    this->activate();
+    next_layer.neurons = this->transposed_weights * this->activated_neurons + this->bias;
+    std::cout << this->activated_neurons;
+    std::cout << "\n";
+    std::cout << next_layer.neurons;
 }
 
 // compute gradient of activation fuction 
 matrix::Matrix Layer::gradients(){
-    const matrix::Matrix gradients;
+    matrix::Matrix gradients = this->neurons;
+    for(auto& z : gradients.matrix){
+        z = this->activation_function_derivative(z);
+    }
     return gradients;
 }
 
-matrix::Matrix Layer::forward(){
-    return this->weights * this->neurons + this->bias
+matrix::Matrix Layer::get_weights(){
+    return this->weights;
 }
 
-// perform forward propagate from layer to next layer
-void Layer::pass_forward(Layer& lhs, Layer& rhs){
-    rhs.neurons = lhs.activate(lhs.forward());
+void Layer::feed(std::vector<double> &input){
+    assert(input.size() == this->fan_in);
+    this->neurons.matrix = input;
+    std::cout << this->neurons;
+}
+/*void pass_forward(Layer& lhs, Layer& rhs){
+    lhs->activate();
+    rhs.neurons = lhs.transposed_weights * lhs.activated_neurons + lhs.bias
+}*/
 }
 
-}
 
 
+/*
 Layer::Layer(int _num_neurons, std::string layer_id){
     this->num_neurons = _num_neurons;
     this->neurons.resize(this->num_neurons);
@@ -113,3 +154,4 @@ std::vector< std::vector<double> > Layer::forward_propagate(){
 void Layer::update_weights(){
     this->weights = element_wise_subtract(this->weights, scalar_multiply(this->learning_rate, this->weights_update));
 }
+*/
