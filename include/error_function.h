ab4b29a4c8ffed10cc082d25a6e17f78bd21470d
Christopher Yoon <cyoon20@cgps.org>
2019-01-04 23:32:05 -0500

    add error functions

---
+++ include/error_function.h
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <assert.h>
+#include "./core_math.h"
+
+namespace optimizer{
+
+// mean squared error
+tensor::Tensor MSE(tensor::Tensor &prediction, tensor::Tensor &actual);
+
+// derivative of mean squared error with respect to output
+tensor::Tensor MSE_derivative(tensor::Tensor &prediction, tensor::Tensor &actual);
+
+// cross entropy error
+tensor::Tensor cross_entropy(tensor::Tensor &prediction, tensor::Tensor &actual);
+
+// derivative of cross entropy error with respect to output
+tensor::Tensor cross_entropy_derivative(tensor::Tensor &prediction, tensor::Tensor &actual);
+
+}
