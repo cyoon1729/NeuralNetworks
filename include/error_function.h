4e9aad7b0a16ce9419162438fa37704bf9785cad
Christopher Yoon <thechrisyoon08@gmail.com>
2019-01-25 21:11:00 -0500

    debug

--- include/error_function.h
+++ include/error_function.h
@@ -11,9 +11,9 @@ tensor::Tensor MSE(tensor::Tensor &prediction, tensor::Tensor &actual);
 tensor::Tensor MSE_derivative(tensor::Tensor &prediction, tensor::Tensor &actual);
 
 // cross entropy error
-tensor::Tensor cross_entropy(tensor::Tensor &prediction, tensor::Tensor &actual);
+//tensor::Tensor cross_entropy(tensor::Tensor &prediction, tensor::Tensor &actual);
 
 // derivative of cross entropy error with respect to output
-tensor::Tensor cross_entropy_derivative(tensor::Tensor &prediction, tensor::Tensor &actual);
+//tensor::Tensor cross_entropy_derivative(tensor::Tensor &prediction, tensor::Tensor &actual);
 
-}
+};
---
+++ include/saNNity.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// include everything
+
+#include "activation_functions.h"
+#include "core_math.h"
+#include "error_function.h"
+#include "network.h"
+#include "optimizer.h"
+#include "layer.h"
