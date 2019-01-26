96d0cfce53cb6e7d2eeb774906b5ceea4f0f9f68
Christopher Yoon <cyoon20@cgps.org>
2019-01-25 21:10:20 -0500

    tests

--- tests/ann.cpp
+++ tests/ann.cpp
@@ -1,5 +1,6 @@
 #include "../include/network.h"
 #include "../include/layer.h"
+#include "../include/optimizer.h"
 #include <vector>
 #include <iostream>
 
@@ -19,6 +20,13 @@ int main(){
     
     std::vector<double> data = {0.3, 0.3, 1.0};
     std::cout << network.forward(data);
+
+    std::vector< std::vector<double> > dataset = {{0.3, 0.3, 1.0}};
+    std::vector< std::vector<double> > correct = {{0.3}};
+    optimizer::VGD optim(network, "MSE");
+    optim.step(network, dataset, correct, 1);
+    //optim.step();
+    std::cout << "ugh";
 
     return 0;
 }
--- tests/run_ann.sh
+++ tests/run_ann.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 ann.cpp ../src/core_math.cpp ../src/layer.cpp ../src/network.cpp -o ann
+g++ -std=c++11 ann.cpp ../src/core_math.cpp ../src/layer.cpp ../src/network.cpp ../src/optimizer.cpp ../src/error_function.cpp -o ann
